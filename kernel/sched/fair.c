0aa66cc390920b35e667ab5db339f85db54b6bb3
Valentin Schneider <valentin.schneider@arm.com>
2021-04-07 23:06:28 +0100

    sched/fair: Introduce a CPU capacity comparison helper
    During load-balance, groups classified as group_misfit_task are filtered
    out if they do not pass
    
      group_smaller_max_cpu_capacity(<candidate group>, <local group>);
    
    which itself employs fits_capacity() to compare the sgc->max_capacity of
    both groups.
    
    Due to the underlying margin, fits_capacity(X, 1024) will return false for
    any X > 819. Tough luck, the capacity_orig's on e.g. the Pixel 4 are
    {261, 871, 1024}. If a CPU-bound task ends up on one of those "medium"
    CPUs, misfit migration will never intentionally upmigrate it to a CPU of
    higher capacity due to the aforementioned margin.
    
    One may argue the 20% margin of fits_capacity() is excessive in the advent
    of counter-enhanced load tracking (APERF/MPERF, AMUs), but one point here
    is that fits_capacity() is meant to compare a utilization value to a
    capacity value, whereas here it is being used to compare two capacity
    values. As CPU capacity and task utilization have different dynamics, a
    sensible approach here would be to add a new helper dedicated to comparing
    CPU capacities.
    
    Also note that comparing capacity extrema of local and source sched_group's
    doesn't make much sense when at the day of the day the imbalance will be
    pulled by a known env->dst_cpu, whose capacity can be anywhere within the
    local group's capacity extrema.
    
    While at it, replace group_smaller_{min, max}_cpu_capacity() with
    comparisons of the source group's min/max capacity and the destination
    CPU's capacity.
    
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Reviewed-by: Qais Yousef <qais.yousef@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Tested-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Link: https://lkml.kernel.org/r/20210407220628.3798191-4-valentin.schneider@arm.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5289,6 +5289,14 @@ static unsigned long capacity_of(int cpu);
 #ifdef CONFIG_MTK_SCHED_EXTENSION
 #define fits_capacity(cap, max) ((cap) * 1280 < (max) * 1024)
 
+/*
+ * The margin used when comparing CPU capacities.
+ * is 'cap1' noticeably greater than 'cap2'Add commentMore actions
+ *
+ * (default: ~5%)
+ */
+#define capacity_greater(cap1, cap2) ((cap1) * 1024 > (cap2) * 1078)
+
 static struct perf_domain *find_pd(struct perf_domain *pd, int cpu)
 {
 	while (pd) {
@@ -9357,28 +9365,6 @@ group_is_overloaded(struct lb_env *env, struct sg_lb_stats *sgs)
 	return false;
 }
 
-/*
- * group_smaller_min_cpu_capacity: Returns true if sched_group sg has smaller
- * per-CPU capacity than sched_group ref.
- */
-static inline bool
-group_smaller_min_cpu_capacity(struct sched_group *sg, struct sched_group *ref)
-{
-	return sg->sgc->min_capacity * capacity_margin <
-						ref->sgc->min_capacity * 1024;
-}
-
-/*
- * group_smaller_max_cpu_capacity: Returns true if sched_group sg has smaller
- * per-CPU capacity_orig than sched_group ref.
- */
-static inline bool
-group_smaller_max_cpu_capacity(struct sched_group *sg, struct sched_group *ref)
-{
-	return sg->sgc->max_capacity * capacity_margin <
-						ref->sgc->max_capacity * 1024;
-}
-
 static inline enum
 group_type group_classify(struct sched_group *group,
 			  struct sg_lb_stats *sgs)
@@ -9538,7 +9524,7 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 	 * internally or be covered by avg_load imbalance (eventually).
 	 */
 	if (sgs->group_type == group_misfit_task &&
-	    (!group_smaller_max_cpu_capacity(sg, sds->local) ||
+	    (!capacity_greater(capacity_of(env->dst_cpu), sg->sgc->max_capacity) ||
 	     !group_has_capacity(env, &sds->local_stat)))
 		return false;
 
@@ -9561,7 +9547,7 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 	 * power/energy consequences are not considered.
 	 */
 	if (sgs->sum_nr_running <= sgs->group_weight &&
-	    group_smaller_min_cpu_capacity(sds->local, sg))
+	    capacity_greater(sg->sgc->min_capacity, capacity_of(env->dst_cpu)))
 		return false;
 
 	/*
@@ -10137,7 +10123,7 @@ static struct rq *find_busiest_queue(struct lb_env *env,
 		 * average load.
 		 */
 		if (env->sd->flags & SD_ASYM_CPUCAPACITY &&
-		    capacity_of(env->dst_cpu) < capacity &&
+		    !capacity_greater((capacity_of(env->dst_cpu)), capacity) &&
 		    rq->nr_running == 1)
 			continue;
 
