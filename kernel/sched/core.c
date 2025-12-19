8dba2e2b4c8ecd9bd06cc1255bb9f96d5b4685a8
Abreuzzs <tauanabrelcelularnovo@gmail.com>
2025-12-19 21:47:34 +0000

    fix bore

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3523,11 +3523,6 @@ void sched_post_fork(struct task_struct *p)
 }
 #endif // CONFIG_SCHED_BORE
 
-void sched_post_fork(struct task_struct *p)
-{
-	uclamp_post_fork(p);
-}
-
 unsigned long to_ratio(u64 period, u64 runtime)
 {
 	if (runtime == RUNTIME_INF)
