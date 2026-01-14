659a8690c934a2508a9e58762477dcb9420bdbbc
Purdianto Efendi <purdianto.efendi@gmail.com>
2026-01-15 00:34:52 +0700

    abreuz project

---
+++ drivers/cpuidle/governors/mtk_menu.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2017 MediaTek Inc.
+ */
+
+#ifndef __MTK_MENU_H__
+#define __MTK_MENU_H__
+
+extern bool is_all_cpu_idle_criteria(void);
+
+#endif /* __MTK_MENU_H__ */
---
+++ drivers/misc/mediatek/apusys/midware/1.1/apusys_dbg.h
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2020 MediaTek Inc.
+ */
+
+#ifndef __APUSYS_DEBUG_H__
+#define __APUSYS_DEBUG_H__
+
+extern bool apusys_dump_force;
+extern bool apusys_dump_skip;
+
+void apusys_reg_dump(void);
+int apusys_dump_show(struct seq_file *sfile, void *v);
+void apusys_dump_reg_skip(int onoff);
+
+#endif
