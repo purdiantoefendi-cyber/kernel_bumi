07913689d8414758cd4fb5b8f341912fb8eace6d
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-02-24 13:04:37 +0800

    fs: susfs: Fix potential race issue for SUS_MAP

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1781,7 +1781,6 @@ static ssize_t pagemap_read(struct file *file, char __user *buf,
 		if (ret)
 			goto out_free;
 		ret = walk_page_range(start_vaddr, end, &pagemap_walk);
-		mmap_read_unlock(mm);
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
 		vma = find_vma(mm, start_vaddr);
 		if (vma && vma->vm_file) {
@@ -1792,6 +1791,7 @@ static ssize_t pagemap_read(struct file *file, char __user *buf,
 			}
 		}
 #endif
+		up_read(&mm->mmap_sem);
 		start_vaddr = end;
 
 		len = min(count, PM_ENTRY_BYTES * pm.pos);
