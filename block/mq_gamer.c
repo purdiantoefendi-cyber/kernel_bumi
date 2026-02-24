a710a7e5f9e739ea6365e5fbc1831253444ff168
purdiantoefendi-cyber <purdianto.efendi@gmail.com>
2026-02-25 02:50:31 +0700

    Update mq_gamer.c

--- block/mq_gamer.c
+++ block/mq_gamer.c
@@ -9,121 +9,100 @@
 #include <linux/init.h>
 #include <linux/spinlock.h>
 #include <linux/blk-mq.h>
-#include <linux/blk-mq-sched.h>
 
 struct gamer_data {
-	struct list_head read_list;
-	struct list_head write_list;
-	spinlock_t lock;
+        struct list_head read_list;
+        struct list_head write_list;
+        spinlock_t lock;
 };
 
-static int gamer_init_queue(struct request_queue *q, struct elevator_type *e)
-{
-	struct gamer_data *gd;
-	struct elevator_queue *eq;
-
-	eq = elevator_alloc(q, e);
-	if (!eq)
-		return -ENOMEM;
-
-	gd = kzalloc_node(sizeof(*gd), GFP_KERNEL, q->node);
-	if (!gd) {
-		kobject_put(&eq->kobj);
-		return -ENOMEM;
-	}
-
-	INIT_LIST_HEAD(&gd->read_list);
-	INIT_LIST_HEAD(&gd->write_list);
-	spin_lock_init(&gd->lock);
-
-	eq->elevator_data = gd;
-	q->elevator = eq;
-	return 0;
-}
-
-static void gamer_exit_queue(struct elevator_queue *e)
-{
-	struct gamer_data *gd = e->elevator_data;
-	kfree(gd);
-}
-
 static void gamer_insert_requests(struct blk_mq_hw_ctx *hctx,
-				  struct list_head *list, bool at_head)
+                                  struct list_head *list, bool at_head)
 {
-	struct gamer_data *gd = hctx->queue->elevator->elevator_data;
-	unsigned long flags;
+        struct gamer_data *gd = hctx->queue->elevator->elevator_data;
+        struct request *rq, *n;
 
-	spin_lock_irqsave(&gd->lock, flags);
-	while (!list_empty(list)) {
-		struct request *rq = list_first_entry(list, struct request, queuelist);
-		list_del_init(&rq->queuelist);
-
-		if (rq_data_dir(rq) == READ) {
-			if (at_head)
-				list_add(&rq->queuelist, &gd->read_list);
-			else
-				list_add_tail(&rq->queuelist, &gd->read_list);
-		} else {
-			if (at_head)
-				list_add(&rq->queuelist, &gd->write_list);
-			else
-				list_add_tail(&rq->queuelist, &gd->write_list);
-		}
-	}
-	spin_unlock_irqrestore(&gd->lock, flags);
+        spin_lock(&gd->lock);
+        list_for_each_entry_safe(rq, n, list, queuelist) {
+                list_del_init(&rq->queuelist);
+                
+                if (rq_data_dir(rq) == READ) {
+                        if (at_head)
+                                list_add(&rq->queuelist, &gd->read_list);
+                        else
+                                list_add_tail(&rq->queuelist, &gd->read_list);
+                } else {
+                        if (at_head)
+                                list_add(&rq->queuelist, &gd->write_list);
+                        else
+                                list_add_tail(&rq->queuelist, &gd->write_list);
+                }
+        }
+        spin_unlock(&gd->lock);
 }
 
 static struct request *gamer_dispatch_request(struct blk_mq_hw_ctx *hctx)
 {
-	struct gamer_data *gd = hctx->queue->elevator->elevator_data;
-	struct request *rq = NULL;
-	unsigned long flags;
+        struct gamer_data *gd = hctx->queue->elevator->elevator_data;
+        struct request *rq = NULL;
 
-	spin_lock_irqsave(&gd->lock, flags);
-	if (!list_empty(&gd->read_list)) {
-		rq = list_first_entry(&gd->read_list, struct request, queuelist);
-		list_del_init(&rq->queuelist);
-	} else if (!list_empty(&gd->write_list)) {
-		rq = list_first_entry(&gd->write_list, struct request, queuelist);
-		list_del_init(&rq->queuelist);
-	}
-	spin_unlock_irqrestore(&gd->lock, flags);
+        spin_lock(&gd->lock);
+        if (!list_empty(&gd->read_list)) {
+                rq = list_first_entry(&gd->read_list, struct request, queuelist);
+                list_del_init(&rq->queuelist);
+        } else if (!list_empty(&gd->write_list)) {
+                rq = list_first_entry(&gd->write_list, struct request, queuelist);
+                list_del_init(&rq->queuelist);
+        }
+        spin_unlock(&gd->lock);
 
-	return rq;
+        return rq;
 }
 
-static bool gamer_has_work(struct blk_mq_hw_ctx *hctx)
+static int gamer_init_sched(struct request_queue *q, struct elevator_type *e)
 {
-	struct gamer_data *gd = hctx->queue->elevator->elevator_data;
+        struct gamer_data *gd;
 
-	return !list_empty_careful(&gd->read_list) ||
-	       !list_empty_careful(&gd->write_list);
+        gd = kzalloc(sizeof(*gd), GFP_KERNEL);
+        if (!gd)
+                return -ENOMEM;
+
+        INIT_LIST_HEAD(&gd->read_list);
+        INIT_LIST_HEAD(&gd->write_list);
+        spin_lock_init(&gd->lock);
+
+        q->elevator->elevator_data = gd;
+        return 0;
+}
+
+static void gamer_exit_sched(struct elevator_queue *e)
+{
+        struct gamer_data *gd = e->elevator_data;
+        kfree(gd);
 }
 
 static struct elevator_type gamer_sched = {
-	.ops = {
-		.mq_ops = &(struct blk_mq_sched_ops){
-			.init_sched         = gamer_init_queue,
-			.exit_sched         = gamer_exit_queue,
-			.insert_requests    = gamer_insert_requests,
-			.dispatch_request   = gamer_dispatch_request,
-			.has_work           = gamer_has_work,
-		},
-	},
-	.elevator_name = "gamer",
-	.elevator_owner = THIS_MODULE,
+        .ops.mq = {
+                .insert_requests = gamer_insert_requests,
+                .dispatch_request = gamer_dispatch_request,
+                .init_sched = gamer_init_sched,
+                .exit_sched = gamer_exit_sched,
+        },
+        .elevator_name = "gamer",
+        .elevator_owner = THIS_MODULE,
+        .uses_mq = true,
 };
 
 static int __init gamer_init(void)
 {
-	pr_info("Gamer I/O Scheduler loaded\n");
-	return elv_register(&gamer_sched);
+        pr_info("Gamer I/O Scheduler loaded\n");
+        return elv_register(&gamer_sched);
 }
 
 static void __exit gamer_exit(void)
 {
-	elv_unregister(&gamer_sched);
-	pr_info("Gamer I/O Scheduler unloaded\n");
+        elv_unregister(&gamer_sched);
+        pr_info("Gamer I/O Scheduler unloaded\n");
 }
 
 module_init(gamer_init);
