385038ee94c8a8eb5ccb3f938a5a0a34e01c0628
purdiantoefendi-cyber <purdianto.efendi@gmail.com>
2026-06-21 16:48:02 +0700

    Add files via upload

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1045,8 +1045,9 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us = cpufreq_policy_transition_delay_us(policy);
-	tunables->down_rate_limit_us = cpufreq_policy_transition_delay_us(policy);
+	/* Paksa delay menjadi 10ms (10000 us) agar super responsif seperti Lagfree */
+	tunables->up_rate_limit_us = 10000;
+	tunables->down_rate_limit_us = 10000;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
