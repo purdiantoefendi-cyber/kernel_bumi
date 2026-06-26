7f61c6a775d3324b9cca66e5f306bf9f52cbdcb3
purdiantoefendi-cyber <purdianto.efendi@gmail.com>
2026-06-26 12:14:04 +0700

    Update mq_performance.c

--- block/mq_performance.c
+++ block/mq_performance.c
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Performance I/O Scheduler (blk-mq) - ANTI FREEZE EDITION
+ * Performance I/O Scheduler (blk-mq) - ULTIMATE STABLE EDITION
  */
-#include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
-#include <linux/blk-mq.h>
+#include <linux/bio.h>
+#include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/init.h>
 #include <linux/spinlock.h>
-#include <linux/list.h>
+#include <linux/blk-mq.h>
 
 #define PERF_READ_QUANTUM 100
 #define PERF_WRITE_QUANTUM 10
 
 struct perf_hctx_data {
+        struct list_head vip_list;
         struct list_head read_list;
         struct list_head write_list;
         spinlock_t lock;
@@ -27,22 +30,23 @@ static void perf_insert_requests(struct blk_mq_hw_ctx *hctx,
                                  struct list_head *list, bool at_head)
 {
         struct perf_hctx_data *hd = hctx->sched_data;
-        struct request *rq, *n;
-        unsigned long flags; /* Penyelamat dari Freeze */
+        struct request *rq;
+        unsigned long flags;
 
         spin_lock_irqsave(&hd->lock, flags);
-        list_for_each_entry_safe(rq, n, list, queuelist) {
+
+        while (!list_empty(list)) {
+                rq = list_first_entry(list, struct request, queuelist);
                 list_del_init(&rq->queuelist);
-                if (rq_data_dir(rq) == READ) {
-                        if (at_head)
-                                list_add(&rq->queuelist, &hd->read_list);
-                        else
-                                list_add_tail(&rq->queuelist, &hd->read_list);
+
+                if (at_head || blk_rq_is_passthrough(rq) || op_is_flush(rq->cmd_flags)) {
+                        if (at_head) list_add(&rq->queuelist, &hd->vip_list);
+                        else list_add_tail(&rq->queuelist, &hd->vip_list);
+                } 
+                else if (rq_data_dir(rq) == READ) {
+                        list_add_tail(&rq->queuelist, &hd->read_list);
                 } else {
-                        if (at_head)
-                                list_add(&rq->queuelist, &hd->write_list);
-                        else
-                                list_add_tail(&rq->queuelist, &hd->write_list);
+                        list_add_tail(&rq->queuelist, &hd->write_list);
                 }
         }
         spin_unlock_irqrestore(&hd->lock, flags);
@@ -56,41 +60,51 @@ static struct request *perf_dispatch_request(struct blk_mq_hw_ctx *hctx)
 
         spin_lock_irqsave(&hd->lock, flags);
 
+        if (!list_empty(&hd->vip_list)) {
+                rq = list_first_entry(&hd->vip_list, struct request, queuelist);
+                list_del_init(&rq->queuelist);
+                goto done;
+        }
+
         if (hd->current_prio == 0) {
                 if (!list_empty(&hd->read_list) && hd->read_count < PERF_READ_QUANTUM) {
                         rq = list_first_entry(&hd->read_list, struct request, queuelist);
+                        list_del_init(&rq->queuelist);
                         hd->read_count++;
                 } else {
                         hd->read_count = 0;
                         if (!list_empty(&hd->write_list)) {
                                 hd->current_prio = 1;
                                 rq = list_first_entry(&hd->write_list, struct request, queuelist);
+                                list_del_init(&rq->queuelist);
                                 hd->write_count = 1;
                         } else if (!list_empty(&hd->read_list)) {
                                 rq = list_first_entry(&hd->read_list, struct request, queuelist);
+                                list_del_init(&rq->queuelist);
                                 hd->read_count = 1;
                         }
                 }
         } else {
                 if (!list_empty(&hd->write_list) && hd->write_count < PERF_WRITE_QUANTUM) {
                         rq = list_first_entry(&hd->write_list, struct request, queuelist);
+                        list_del_init(&rq->queuelist);
                         hd->write_count++;
                 } else {
                         hd->write_count = 0;
                         if (!list_empty(&hd->read_list)) {
                                 hd->current_prio = 0;
                                 rq = list_first_entry(&hd->read_list, struct request, queuelist);
+                                list_del_init(&rq->queuelist);
                                 hd->read_count = 1;
                         } else if (!list_empty(&hd->write_list)) {
                                 rq = list_first_entry(&hd->write_list, struct request, queuelist);
+                                list_del_init(&rq->queuelist);
                                 hd->write_count = 1;
                         }
                 }
         }
 
-        if (rq)
-                list_del_init(&rq->queuelist);
-
+done:
         spin_unlock_irqrestore(&hd->lock, flags);
         return rq;
 }
@@ -102,9 +116,10 @@ static bool perf_has_work(struct blk_mq_hw_ctx *hctx)
         unsigned long flags;
 
         spin_lock_irqsave(&hd->lock, flags);
-        has_work = !list_empty(&hd->read_list) || !list_empty(&hd->write_list);
+        has_work = !list_empty(&hd->vip_list) || 
+                   !list_empty(&hd->read_list) || 
+                   !list_empty(&hd->write_list);
         spin_unlock_irqrestore(&hd->lock, flags);
-
         return has_work;
 }
 
@@ -113,6 +128,7 @@ static int perf_init_hctx(struct blk_mq_hw_ctx *hctx, unsigned int hctx_idx)
         struct perf_hctx_data *hd = kzalloc_node(sizeof(*hd), GFP_KERNEL, hctx->numa_node);
         if (!hd) return -ENOMEM;
 
+        INIT_LIST_HEAD(&hd->vip_list);
         INIT_LIST_HEAD(&hd->read_list);
         INIT_LIST_HEAD(&hd->write_list);
         spin_lock_init(&hd->lock);
@@ -131,11 +147,11 @@ static void perf_exit_hctx(struct blk_mq_hw_ctx *hctx, unsigned int hctx_idx)
 
 static struct elevator_type perf_sched = {
         .ops.mq = {
+                .init_hctx = perf_init_hctx,
+                .exit_hctx = perf_exit_hctx,
                 .insert_requests = perf_insert_requests,
                 .dispatch_request = perf_dispatch_request,
                 .has_work = perf_has_work,
-                .init_hctx = perf_init_hctx,
-                .exit_hctx = perf_exit_hctx,
         },
         .elevator_name = "performance", 
         .elevator_owner = THIS_MODULE,
