105508915d5d1864b35cde5048295c4f418bbe09
g4bri3ldll <gabrielalves.dasilva.0983@gmail.com>
2026-07-02 21:01:28 -0300

    Add manual hook for ksu in fstat function

--- fs/stat.c
+++ fs/stat.c
@@ -547,6 +547,9 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
+#ifdef CONFIG_KSU_MANUAL_HOOK // 32-bit su
+	ksu_handle_stat(&dfd, &filename, &flag); 
+#endif
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
 		return error;
