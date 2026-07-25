af096a4b9f77d8b7ddc1aac19b7ad2da1670d6cb
purdiantoefendi-cyber <purdianto.efendi@gmail.com>
2026-07-25 12:36:05 +0700

    Update cpufreq_schedutil_plus.c

--- kernel/sched/cpufreq_schedutil_plus.c
+++ kernel/sched/cpufreq_schedutil_plus.c
@@ -35,8 +35,8 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	if (max > 0)
 		load_pct = (util * 100) / max;
 
-	if (load_pct >= 70) {
-		/* Threshold 70%: Titik seimbang performa, langsung gas pol ke Max Freq! */
+	if (load_pct >= 75) {
+		/* Threshold 75%: Titik seimbang performa, langsung gas pol ke Max Freq! */
 		freq = policy->max;
 	} else if (load_pct <= 35) {
 		/* Threshold 35%: Beban ringan, lebih cepat drop ke Mode Hemat (Min Freq) */
