6760fb1bdcd1b4b5124c170249df2b9fc7d31fa8
purdiantoefendi-cyber <purdianto.efendi@gmail.com>
2026-08-10 07:43:51 +0700

    Comment out TLB remove function call

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -32,7 +32,7 @@ static void tlb_remove_table_smp_sync(void *arg)
 
 static void tlb_remove_table_sync_one(void)
 {
-        smp_call_function(tlb_remove_table_smp_sync, NULL, 1);
+// Do nothing
 }
 #endif
 
